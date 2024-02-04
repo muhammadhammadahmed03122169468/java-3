4e037a9897e7af2c6289943822d8f6190fde7dca
muhammadhammadahmed03122169468 <hammad03122169468@gmail.com>
2024-02-04 15:41:48 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var age=+prompt("Enter your age");
+var aa="I am"
+var bb="Years old"
+
+
+alert(aa +" "+ +age+" "+  bb)
+
+
+var time=+prompt("Enter your visit time");
+var c="you have visited this site"
+var d="times"
+
+
+alert(c +" "+ +time+" "+  d)
+
+
+var e=+prompt("Enter your birth year");
+var f="My birth year is"
+
+
+
+
+document.write(f +" "+ +e);
+
+
+var vi= prompt("Visitor's Name");
+var pr= +prompt("pices in number");
+var com= prompt("Store Name");
+var or="ordered"
+var sh="T-Shirt"
+var clo="Clothing Store"
+
+
+document.write(vi  +" "+ or+" "+  pr +" "+ sh +" "+ com+" "+clo);
